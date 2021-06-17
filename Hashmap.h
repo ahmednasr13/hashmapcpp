8992232daa5fc907f4d85e45b2d18fce1369290b
Nasr <ahmednasrgomaa13@gmail.com>
2021-06-17 20:30:55 +0200

    Update Hashmap.h

--- Hashmap.h
+++ Hashmap.h
@@ -9,10 +9,13 @@
 #define HASHMAP_H_
 
 #include <string>
+#include <vector>
 
 template<typename K, typename V>
 class MapNode
 {
+	template<typename Y, typename Z>
+	friend class mymaptemplate;
 	template<typename Y, typename Z>
 	friend class mymap;
 
@@ -32,13 +35,15 @@ private:
     }
 };
 
-template<typename T>
+template<typename K>
 class hash
 {
 	template<typename Y, typename Z>
+	friend class mymaptemplate;
+	template<typename Y, typename Z>
 	friend class mymap;
 
-	static unsigned int hashFunction(const T &key, unsigned int bucketSize)
+	static unsigned int hashFunction(const K &key, unsigned int bucketSize)
 	{
 		return static_cast<unsigned int>(key) % bucketSize;
 	}
@@ -47,6 +52,8 @@ class hash
 template<>
 class hash<std::string>
 {
+	template<typename Y, typename Z>
+	friend class mymaptemplate;
 	template<typename Y, typename Z>
 	friend class mymap;
 
@@ -66,13 +73,14 @@ class hash<std::string>
 };
 
 template<typename K, typename V>
-class mymap
+class mymaptemplate
 {
-private:
+protected:
 	unsigned int count;
 	unsigned int numBuckets;
 	MapNode<K, V>** buckets;
 	bool checkLoadFactor;
+	double maxLoadFactor;
 
 	unsigned int getBucketIndex(const K &key) const
 	{
@@ -80,13 +88,13 @@ private:
 	}
 
 public:
-	mymap() : count{0}, numBuckets{5}, buckets{new MapNode<K, V>*[numBuckets]}, checkLoadFactor{true}
+	mymaptemplate(unsigned int numbuckets, double maxLoadFactor) : count{0}, numBuckets{numbuckets}, buckets{new MapNode<K, V>*[numBuckets]}, checkLoadFactor{true}, maxLoadFactor{maxLoadFactor}
 	{
 		for(unsigned int i = 0; i < numBuckets; i++)
 			buckets[i] = nullptr;
 	}
 
-	~mymap()
+	virtual ~mymaptemplate()
 	{
 		for(unsigned int i = 0; i < numBuckets; i++)
 			delete buckets[i];
@@ -94,25 +102,7 @@ public:
 		delete[] buckets;
 	}
 
-	V &operator[](const K &key)
-	{
-		if(!isExist(key))
-		{
-			V temp;
-			insert(key, temp);
-		}
-
-		V *value;
-
-		for(MapNode<K, V>* head = buckets[getBucketIndex(key)]; head != nullptr; head = head->next)
-			if(head->key == key)
-			{
-				value = &head->value;
-				break;
-			}
-
-		return *value;
-	}
+	virtual V &operator[](const K &key) = 0;
 
 	void insert(const K &key, const V &value)
 	{
@@ -138,9 +128,7 @@ public:
 
 		if(checkLoadFactor)
 		{
-			const double maxLoadFactor = 0.7;
-
-			if(getLoadFactor() >= maxLoadFactor)
+			if(getLoadFactor() > maxLoadFactor)
 			{
 				checkLoadFactor = false;
 				rehash();
@@ -148,49 +136,9 @@ public:
 		}
 	}
 
-	V remove(const K &key)
-	{
-		unsigned int bucketIndex = getBucketIndex(key);
+	virtual V remove(const K &key) = 0;
 
-		for(MapNode<K, V>* head = buckets[bucketIndex], *prev = nullptr; head != nullptr; prev = head, head = head->next)
-		{
-			if(head->key == key)
-			{
-				if(prev != nullptr)
-					prev->next = head->next;
-				else
-					buckets[bucketIndex] = head->next;
-
-				V currentValue = head->value;
-
-				head->next = nullptr;
-				delete head;
-
-				count--;
-
-				return currentValue;
-			}
-		}
-
-		return 0;
-	}
-
-	V getValue(const K &key) const
-	{
-		for(MapNode<K, V>* head = buckets[getBucketIndex(key)]; head != nullptr; head = head->next)
-			if(head->key == key)
-				return head->value;
-
-		try
-		{
-			return 0;
-		}
-		catch(...)
-		{
-			V temp;
-			return temp;
-		}
-	}
+	virtual V getValue(const K &key) const = 0;
 
 	unsigned int size() const
 	{
@@ -242,6 +190,207 @@ public:
 				return true;
 
 		return false;
+	}
+
+	std::vector<K> getAllKeys() const
+	{
+		std::vector<K> keys;
+
+		for(unsigned int i = 0; i < numBuckets; i++)
+			for(MapNode<K, V>* head = buckets[i]; head != nullptr; head = head->next)
+				keys.push_back(head->key);
+
+		return keys;
+	}
+
+	unsigned int getBucketsCount() const
+	{
+		return numBuckets;
+	}
+
+	void setMaxLoadFactor(double maxLoadFactor)
+	{
+		this->maxLoadFactor = maxLoadFactor;
+	}
+};
+
+template<typename K, typename V>
+class mymap : public mymaptemplate<K, V>
+{
+public:
+	mymap(unsigned int numBuckets = 5, double maxLoadFactor = 0.7) : mymaptemplate<K, V>(numBuckets, maxLoadFactor)
+	{
+
+	}
+	virtual ~mymap()
+	{
+
+	}
+
+	virtual V &operator[](const K &key)
+	{
+		if(!mymaptemplate<K, V>::isExist(key))
+			mymaptemplate<K, V>::insert(key, 0);
+
+		V *value;
+
+		for(MapNode<K, V>* head = mymaptemplate<K, V>::buckets[mymaptemplate<K, V>::getBucketIndex(key)]; head != nullptr; head = head->next)
+			if(head->key == key)
+			{
+				value = &head->value;
+				break;
+			}
+
+		//it's impossible to be not found as the element is already inserted, so it is impossible to dereference value variable without initializing it
+		return *value;
+	}
+
+	virtual V remove(const K &key)
+		{
+			unsigned int bucketIndex = mymaptemplate<K, V>::getBucketIndex(key);
+
+			for(MapNode<K, V>* head = mymaptemplate<K, V>::buckets[bucketIndex], *prev = nullptr; head != nullptr; prev = head, head = head->next)
+			{
+				if(head->key == key)
+				{
+					if(prev != nullptr)
+						prev->next = head->next;
+					else
+						mymaptemplate<K, V>::buckets[bucketIndex] = head->next;
+
+					V currentValue = head->value;
+
+					head->next = nullptr;
+					delete head;
+
+					mymaptemplate<K, V>::count--;
+
+					return currentValue;
+				}
+			}
+
+			return 0;
+		}
+
+	virtual V getValue(const K &key) const
+	{
+		for(MapNode<K, V>* head = mymaptemplate<K, V>::buckets[mymaptemplate<K, V>::getBucketIndex(key)]; head != nullptr; head = head->next)
+			if(head->key == key)
+				return head->value;
+
+		return 0;
+	}
+
+	std::pair<K, V> max_element() const
+	{
+		std::pair<K, V> max;
+
+		if(mymaptemplate<K, V>::count == 0)
+			return max;
+
+		bool foundFirstElement = false;
+
+		for(unsigned int i = 0; i < mymaptemplate<K, V>::numBuckets; i++)
+			for(MapNode<K, V>* head = mymaptemplate<K, V>::buckets[i]; head != nullptr; head = head->next)
+				if(!foundFirstElement || head->value > max.second)
+				{
+					max.first = head->key;
+					max.second = head->value;
+
+					foundFirstElement = true;
+				}
+
+		return max;
+	}
+
+	std::pair<K, V> min_element() const
+	{
+		std::pair<K, V> min;
+
+		if(mymaptemplate<K, V>::count == 0)
+			return min;
+
+		bool foundFirstElement = false;
+
+		for(unsigned int i = 0; i < mymaptemplate<K, V>::numBuckets; i++)
+			for(MapNode<K, V>* head = mymaptemplate<K, V>::buckets[i]; head != nullptr; head = head->next)
+				if(!foundFirstElement || head->value < min.second)
+				{
+					min.first = head->key;
+					min.second = head->value;
+
+					foundFirstElement = true;
+				}
+
+		return min;
+	}
+};
+
+template<typename K>
+class mymap<K, std::string> : public mymaptemplate<K, std::string>
+{
+public:
+	mymap(unsigned int numbuckets = 5, double maxLoadFactor = 0.7) : mymaptemplate<K, std::string>(numbuckets, maxLoadFactor)
+	{
+
+	}
+	virtual ~mymap()
+	{
+
+	}
+
+	virtual std::string &operator[](const K &key)
+	{
+		if(!mymaptemplate<K, std::string>::isExist(key))
+			mymaptemplate<K, std::string>::insert(key, "");
+
+		std::string *value;
+
+		for(MapNode<K, std::string>* head = mymaptemplate<K, std::string>::buckets[mymaptemplate<K, std::string>::getBucketIndex(key)]; head != nullptr; head = head->next)
+			if(head->key == key)
+			{
+				value = &head->value;
+				break;
+			}
+
+		//it's impossible to be not found as the element is already inserted, so it is impossible to dereference value variable without initializing it
+		return *value;
+	}
+
+	virtual std::string remove(const K &key)
+	{
+		unsigned int bucketIndex = mymaptemplate<K, std::string>::getBucketIndex(key);
+
+		for(MapNode<K, std::string>* head = mymaptemplate<K, std::string>::buckets[bucketIndex], *prev = nullptr; head != nullptr; prev = head, head = head->next)
+		{
+			if(head->key == key)
+			{
+				if(prev != nullptr)
+					prev->next = head->next;
+				else
+					mymaptemplate<K, std::string>::buckets[bucketIndex] = head->next;
+
+				std::string currentValue = head->value;
+
+				head->next = nullptr;
+				delete head;
+
+				mymaptemplate<K, std::string>::count--;
+
+				return currentValue;
+			}
+		}
+
+		return "";
+	}
+
+	virtual std::string getValue(const K &key) const
+	{
+		for(MapNode<K, std::string>* head = mymaptemplate<K, std::string>::buckets[mymaptemplate<K, std::string>::getBucketIndex(key)]; head != nullptr; head = head->next)
+			if(head->key == key)
+				return head->value;
+
+		return "";
 	}
 };
 
